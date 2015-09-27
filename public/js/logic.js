ee71b73b2cd38791d76879bdf80538bd4ad3e5e4
Vishal R <vishivish18@gmail.com>
2015-09-27 22:46:14 +0530

    integrating node

---
+++ public/js/logic.js
@@ -0,0 +1,8 @@
+console.log("hello");
+var flag = 0;
+
+$(document).ready(function() { 		
+	$( ".box" ).click(function($el,$ev) {
+		
+	});
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+
+http.listen(3000,function(){
+console.log('listening on *3000');
+});
